94f639dca6948401a1883dfb5f0810cc76907fce
mdsujan-mriddha <mdsujan134523@gmail.com>
2024-02-11 18:51:17 +0600

    name as productName

--- model/productModel.js
+++ model/productModel.js
@@ -2,7 +2,7 @@
 const mongoose = require('mongoose');
 
 const productSchema = new mongoose.Schema({
-    name: {
+    productName: {
         type: String,
         required: [true, "Please Enter product Name"],
         trim: true,
