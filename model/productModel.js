1a9c3f19d025881bb4fbc131f1ba9f030e0b9722
mdsujan-mriddha <mdsujan134523@gmail.com>
2024-02-11 16:08:52 +0600

    remove requie image

--- model/productModel.js
+++ model/productModel.js
@@ -20,11 +20,11 @@ const productSchema = new mongoose.Schema({
         {
             public_id: {
                 type: String,
-                required: true,
+               
             },
             url: {
                 type: String,
-                required: true,
+               
             },
         },
     ],
