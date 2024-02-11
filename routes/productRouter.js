4160918222307c9321ddd9d7f5d3d5f67ea46607
mdsujan-mriddha <mdsujan134523@gmail.com>
2024-02-11 12:36:11 +0600

    get product without auth user

--- routes/productRouter.js
+++ routes/productRouter.js
@@ -11,15 +11,15 @@ const {
 const { isAuthenticatedUser, authorizeRoles } = require("../middleware/auth");
 
 const router = express.Router();
-router.route("/new/product").post(isAuthenticatedUser, createProduct);
+router.route("/new/product").post(createProduct);
 // get all products
-router.route("/products").get(isAuthenticatedUser, getAllProducts);
+router.route("/products").get(getAllProducts);
 // update product 
 router.route("/update/product/:id").put(isAuthenticatedUser, authorizeRoles("admin"), updateProduct);
 // deleteProduct
 router.route("/delete/product/:id").delete(isAuthenticatedUser, authorizeRoles("admin"), deleteProduct);
 // get product details 
-router.route("/product/:id").get(isAuthenticatedUser, getProductDetails);
+router.route("/product/:id").get(getProductDetails);
 
 
 module.exports = router;
