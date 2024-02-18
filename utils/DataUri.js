1df4591332ce2160d7cdb54ccf22afacdf7ccc46
mdsujan-mriddha <mdsujan134523@gmail.com>
2024-02-18 18:13:08 +0600

    update

--- utils/DataUri.js
+++ utils/DataUri.js
@@ -3,6 +3,6 @@ const DataUriParser = require("datauri/parser");
 const path = require("path");
 exports.getDataUri = (file) => {
     const parser = new DataUriParser();
-    const extName = path.extname(file?.originalname).toString();
-    return parser.format(extName, file.buffer);
+    const extName = path?.extname(file?.originalname).toString();
+    return parser.format(extName, file?.buffer);
 };
