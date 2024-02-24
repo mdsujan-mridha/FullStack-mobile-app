8dd66f575e79a6bad12383c9489c75aa6939e454
mdsujan-mriddha <mdsujan134523@gmail.com>
2024-02-25 04:22:54 +0600

    fixed logout

--- config/dbConnection.js
+++ config/dbConnection.js
@@ -5,13 +5,14 @@ const database = (module.exports = () => {
         useNewUrlParser: true,
         useUnifiedTopology: true,
     }
+    
     try {
         mongoose.set('strictQuery', true);
         mongoose.connect(`mongodb+srv://${process.env.DB_NAME}:${process.env.DB_PASS}@cluster0.jf92v2w.mongodb.net/?retryWrites=true&w=majority`,
             connectionParams
         );
         console.log("🟰⏩▶️=>Database connected😎😍🆗");
-        console.log("🟰⏩▶️Connected with MongoDB-👍👌😍");
+        console.log("🟰⏩▶️ Connected with MongoDB-👍👌😍");
     } catch (error) {
         console.log(error);
         console.log("❌🛑🆘Database not connected,Try again😒😥😭");
